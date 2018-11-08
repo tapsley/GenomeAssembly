4ec68516cc983ab36a5a9b8fa2240e37d7863e06
trevorlyman <trevorlyman94@gmail.com>
2018-11-08 11:14:13 -0700

    Moved some stuff around. Finding Contigs in the largest dataset is too slow.

--- GenomeAssembly/GenomeAssembler.py
+++ GenomeAssembly/GenomeAssembler.py
@@ -1,3 +1,8 @@
+from random import randint
+
+import sys
+import time
+
 from lib import fasta_to_string
 from lib import generateContigs
 from lib import construct_graph
@@ -6,42 +11,100 @@ from lib import find_start_node
 from lib import find_eulerian_path
 from lib import window
 
-def n50(data):
-    return -1
 
-def build_kmer_list(fata_lines,k,  error_cutoff):
-    return []
+class assembly():
+    def __init__(self, kmers):
+        self.kmers = kmers
 
-def random_search():
-    pass
+        self.contig_len = -1
+        self.genome = ""
+
+        # Contigs
+        #self.contigs = generateContigs(self.kmers)
+        self.contigs = ["ACGT"]
+        self.contig_len = len(self.contigs)
+        self.n50 = calc_n50(self.contigs)
+
+        # Genome
+        self.genome = assemble_genome(self.kmers)
+        self.genome_len = len(self.genome)
+
+
+def calc_n50(contigs):
+    contigs.sort(key=len)
+    # Even
+    if len(contigs) % 2 == 0:
+        left = int((len(contigs)/2))
+        right = left + 1
+        return int((len(contigs[left]) + len(contigs[right]) / 2))
+    else: # Odd
+        median = int(len(contigs) / 2)
+        return len(contigs[median])
+
+def fasta_to_kmer_counts(fasta_lines, k):
+    kmer_counts = {}
+
+    for line in fasta_lines:
+        line_kmers = list(window(line, k))
+        for kmer in line_kmers:
+            if kmer not in kmer_counts:
+                kmer_counts[kmer] = 1
+            else:
+                kmer_counts[kmer] += 1
+
+    return kmer_counts
+
+def build_kmer_list(fasta_lines,k , error_cutoff):
+
+    kmer_counts = fasta_to_kmer_counts(fasta_lines, k)
+
+    kmers = []
+    for kmer, count in kmer_counts.items():
+        if count > error_cutoff:
+            kmers.append(kmer)
+
+    return kmers
+
+def kmer_count_average(fasta_lines):
+    kmer_counts = fasta_to_kmer_counts(fasta_lines)
+
+    sum = 0
+    for _, count in kmer_counts.items():
+        sum += count
+
+    return sum / len(kmer_counts)
+
+def random_search(fasta_lines, count=100):
+    #average_kmer_count = kmer_count_average(fasta_lines)
+    err_min = 0
+    err_max = 0
+
+    k_min = 5
+    k_max = 30
+
+    best_n50 = -1
+    best_k = 0
+    best_err = 0
+    best_genome = ""
+
+    for i in range(count):
+        local_n50 = 0
+        err = randint(err_min, err_max)
+        k = randint(k_min, k_max)
+        kmers = build_kmer_list(fasta_lines, k , err)
+        result = assembly(kmers)
+
+        if best_n50 < result.n50:
+            best_n50 = result.n50
+            best_k = k
+            best_err = err
+            best_genome = result.genome
+
+    return best_n50, best_k, best_err, best_genome
+
+
 
 def assemble_genome(kmers):
-    return ""
-
-def main():
-    dna = fasta_to_string("files/synthetic.noerror.small.fasta")
-    solutionFile = open("files/example.solution.fasta", 'r')
-    solution = solutionFile.readline()
-    solutionFile.close()
-    #print "\n".join(dna)
-
-
-
-    kmers = set()
-    k = 20
-    for d in dna:
-        kmers.update(list(window(d, k)))
-
-
-    contigs = generateContigs(list(kmers))
-
-    print("Contigs")
-    for kmer in contigs:
-        print(kmer)
-
-
-    print()
-    print("Assembled Genomre")
     graph = construct_graph(kmers)
     degrees = calc_degrees(graph)
     start_node = find_start_node(degrees)
@@ -51,10 +114,29 @@ def main():
     for node in eulerian_path:
         assembled_genome = assembled_genome + node[-1:]
 
-    print(assembled_genome)
-    print(len(assembled_genome))
-    #print(solution)
+    return assembled_genome
 
+def main():
+    fasta_lines = fasta_to_string("files/synthetic.noerror.small.fasta")
+    #fasta_lines = fasta_to_string("files/example.data.fasta")
+    fasta_lines = fasta_to_string("files/real.error.large.fasta")
+
+
+    start = time.time()
+    n50, k, err, genome = random_search(fasta_lines, 1)
+    print("n50: ", n50)
+    print("k:  ", k)
+    print("err: ", err)
+    print("genome: ", genome)
+
+    print("")
+    print(time.time() - start)
+
+
+
+
+
+sys.setrecursionlimit(1000000000)
 main()
 
 # Todo: calc n50
